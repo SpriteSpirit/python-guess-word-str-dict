d89cd16f66c2631b925cb70fdf0f641b2ebd2f83
Sprite_Spirit <Sprite.spirit.t.me@gmail.com>
2024-11-17 01:19:03 +0300

    Создана функция для запроса уровня сложности у пользователя

--- src/functions.py
+++ src/functions.py
@@ -48,3 +48,23 @@ def role(text: str = "", members: int = 2) -> None or str:
         user = input(f"{user} ")
 
         return user
+
+
+def get_user_choice_level() -> str:
+    """
+    Запрашивает у пользователя уровень сложности.
+    :return: Возвращает строковое значение ключа
+    """
+    role("Выберите уровень сложности.", 1)
+    user_choice = role(f"{', '.join(DIFFICULTY_LEVELS.keys()).capitalize()}.", 2)
+
+    if isinstance(user_choice, str):
+        user_choice = user_choice.strip().lower()
+
+        if user_choice not in DIFFICULTY_LEVELS:
+            role("Неверный ввод.", 1)
+            role(f"Пожалуйста, введите {', '.join(DIFFICULTY_LEVELS.keys()).title()}.", 1)
+            return get_user_choice_level()
+        else:
+            role(f"Выбран {user_choice} уровень сложности, мы предложим 5 слов, подберите перевод.", 1)
+            return DIFFICULTY_LEVELS[user_choice]
